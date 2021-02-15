b70d32d6f16c00ed99de61529984a8f24c5ad9c1
therainmaiker <57842299+therainmaiker@users.noreply.github.com>
2021-02-15 16:02:35 +0100

    entitiy framework added

---
+++ last one/Model/Student.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace last_one.Model
+{
+    public class Student
+    {
+
+        
+
+        public int Idstudent { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public int CIN { get; set; }
+
+        public string Address { get; set; }
+
+
+    }
+}
